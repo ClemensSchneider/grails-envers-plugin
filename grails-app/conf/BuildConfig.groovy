a010060bc90f05d072b3e11bb2e8e5001cf2b316
Clemens <clemens.schneider@gmx.de>
2012-02-23 17:42:20 +0100

    added explicit target and source level
    - error was thrown when using jdk7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,6 +18,9 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
